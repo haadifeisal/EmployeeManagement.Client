c83eae38cc16d733b217ce85bb7d99feb8f37524
Haadi Feisal <haadii_94@hotmail.com>
2023-03-08 16:25:27 +0100

    Initial commit

---
+++ EmployeeManagement.Client/src/app/view-departments/view-departments.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ViewDepartmentsComponent } from './view-departments.component';
+
+describe('ViewDepartmentsComponent', () => {
+  let component: ViewDepartmentsComponent;
+  let fixture: ComponentFixture<ViewDepartmentsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ViewDepartmentsComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ViewDepartmentsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ EmployeeManagement.Client/src/app/view-departments/view-departments.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-departments',
+  templateUrl: './view-departments.component.html',
+  styleUrls: ['./view-departments.component.css']
+})
+export class ViewDepartmentsComponent {
+
+}
